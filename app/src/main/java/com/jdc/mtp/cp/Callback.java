0edbcae6a28ecd08c0b7e54a7c762c1e59f87186
jhondev-code <jhondev.code@gmail.com>
2021-01-19 23:15:04 -0500

    Create Callback.java

---
+++ app/src/main/java/com/jdc/mtp/cp/Callback.java
@@ -0,0 +1,16 @@
+package com.jdc.mtp.cp;
+
+/**
+ * Esta interfaz es usada para ejecutar codigo despues de una accion.
+ *
+ * @version 1.0.0
+ * @author Jhon
+ **/
+public interface Callback {
+
+    /**
+     * El codigo a ejecutar se implementa en este metodo.
+     **/
+    void execute();
+
+}
