765aea829e6c72ed9a6295e4cbdda49564db7708
jhondev-code <jhondev.code@gmail.com>
2021-01-19 22:58:49 -0500

    Create ContentRequester.java

---
+++ app/src/main/java/com/jdc/mtp/cp/ContentRequester.java
@@ -0,0 +1,16 @@
+package com.jdc.mtp.cp;
+
+/**
+ * Esta interfaz posee un metodo el cual es llamado cuando el contenido del portapapeles ha cambiado.
+ *
+ * @author Jhon
+ * @version 1.0.0
+ **/
+public interface ContentRequester {
+
+    /**
+     * Es llamado cuando el contenido del portapapeles cambia
+     **/
+    void onChanged(Object content);
+
+}
