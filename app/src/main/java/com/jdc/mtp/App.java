947cc1e89a69a141af91d6a1e5eb367acbbf484b
jhondev-code <jhondev.code@gmail.com>
2021-01-19 22:27:25 -0500

    He actualizado las dependencias

--- app/src/main/java/com/jdc/mtp/App.java
+++ app/src/main/java/com/jdc/mtp/App.java
@@ -3,7 +3,6 @@
  */
 package com.jdc.mtp;
 
-import javax.swing.*;
 import java.awt.*;
 
 public class App {
@@ -13,11 +12,6 @@ public class App {
             try {
                 /* the code */
                 // user.dir
-                JFrame frame = new JFrame("SVG test");
-                frame.setDefaultCloseOperation(WindowConstants.EXIT_ON_CLOSE);
-                frame.setSize(800, 500);
-                frame.setLocationRelativeTo(null);
-                frame.setVisible(true);
             } catch (Exception ex) {
                 ex.printStackTrace();
             }
