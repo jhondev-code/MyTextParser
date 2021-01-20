80dfeb32278c031481b61d00abfa6f3ef9b6df06
jhondev-code <jhondev.code@gmail.com>
2021-01-20 00:28:41 -0500

    Estados del monitor
    Agregué los metodos para verificar los estados del monitor

--- app/src/main/java/com/jdc/mtp/cp/Monitor.java
+++ app/src/main/java/com/jdc/mtp/cp/Monitor.java
@@ -5,22 +5,49 @@ package com.jdc.mtp.cp;
  **/
 public abstract class Monitor {
 
+    private boolean run = false;
+    private boolean pause = false;
+    private boolean stop = false;
+
     /* Constructor por defecto */
     public Monitor() {/* empty constructor */}
 
     /**
      * Este metodo se usa para iniciar el monitor
      **/
-    public void start(CallBack back, CallLater later) {}
+    public void start(CallBack back, CallLater later) {
+        this.run = true;
+        this.pause = false;
+        this.stop = false;
+    }
 
     /**
      * Este mtodo se usa para pausar el monitor
      **/
-    public void pause(CallBack back, CallLater later) {}
+    public void pause(CallBack back, CallLater later) {
+        this.pause = true;
+        this.run = false;
+        this.stop = false;
+    }
 
     /**
      * Este metodo se usa para detener al monitor
      **/
-    public void stop(CallBack back, CallLater later) {}
+    public void stop(CallBack back, CallLater later) {
+        this.run = false;
+        this.pause = false;
+        this.stop = true;
+    }
 
+    public boolean isPaused() {
+        return this.pause;
+    }
+
+    public boolean isRunning() {
+        return this.run;
+    }
+
+    public boolean isStopped() {
+        return this.stop;
+    }
 }
