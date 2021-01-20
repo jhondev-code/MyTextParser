a7fa7d5b27d6a3eb681b4cd87ad77b97bb5cf7fe
jhondev-code <jhondev.code@gmail.com>
2021-01-19 23:56:38 -0500

    Actualicé ContentRequester.java

--- app/src/main/java/com/jdc/mtp/cp/ContentRequester.java
+++ app/src/main/java/com/jdc/mtp/cp/ContentRequester.java
@@ -1,5 +1,7 @@
 package com.jdc.mtp.cp;
 
+import java.awt.datatransfer.Clipboard;
+
 /**
  * Esta interfaz posee un metodo el cual es llamado cuando el contenido del portapapeles ha cambiado.
  *
@@ -11,6 +13,6 @@ public interface ContentRequester {
     /**
      * Es llamado cuando el contenido del portapapeles cambia
      **/
-    void onChanged(ClipboardActions actions);
+    void onChanged(Object content, Clipboard clipboard);
 
 }
